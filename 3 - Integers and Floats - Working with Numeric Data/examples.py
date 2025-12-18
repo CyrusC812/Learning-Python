d92e441ec0538ce435f2b2ab34c77759db618eb5
Cyrus Chung <chung.cyrus.0@gmail.com>
2025-12-18 07:05:14 +0000

    Completed Video 3: Integers and Floats - Working with Numeric Data and example.py
    Completed Video 3: Integers and Floats - Working with Numeric Data and example.py

---
+++ 3 - Integers and Floats - Working with Numeric Data/examples.py
@@ -0,0 +1,56 @@
+# type(num)
+num = 3.14
+print(type(num))
+num = 3
+print(type(num))
+
+# Arithmetic Operators:
+
+# Addition:       3 + 2
+print(3 + 2)
+# Subtraction:    3 - 2
+print(3 - 2)
+# Multiplication: 3 * 2
+print(3 * 2)
+# Division:       3 / 2
+print(3 / 2)
+# Floor Division: 3 // 2
+print(3 // 2)
+# Modulus:        3 % 2
+print(3 % 2)
+
+# Comparisons: - returns boolean
+
+# Equal:            3 == 2
+print(3==2)
+# Not Equal:        3 != 2
+print(3!=2)
+# Greater Than:     3 > 2
+print(3>2)
+# Less Than:        3 < 2
+print(3<2)
+# Greater or Equal: 3 >= 2
+print(3>=2)
+# Less or Equal:    3 <= 2
+print(3<=2)
+
+
+# Incrementing
+
+num = 2
+num = num + 1
+print(num)
+
+# Absolute Value
+num = -3
+print(abs(num))
+
+# Rounding - 2 decimal palces
+num = 3.1415
+print(round(num,2))
+
+# Casting
+num_1 = "154"
+num_2 = "100"
+
+print(int(num_1)-int(num_2))
