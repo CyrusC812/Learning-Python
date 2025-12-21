5e3b2bf751a649053776d38bcc48987441f34a12
Cyrus Chung <chung.cyrus.0@gmail.com>
2025-12-21 22:26:51 +0000

    continued video 11

--- 11 - File Objects - Reading and Writing to Files/examples.py
+++ 11 - File Objects - Reading and Writing to Files/examples.py
@@ -1,3 +1,18 @@
-with open('test.txt','r') as var:
-    g = var.readline()
-    print(g)
+import time
+with open('C:/Users/chung/OneDrive/Documents/GitHub/Learning-Python/11 - File Objects - Reading and Writing to Files/test.txt','r') as var: # Loops through each line of the test.txt and prints them out one by one
+    for line in var:
+        print(line, end="")
+print("\n\n")
+with open('11 - File Objects - Reading and Writing to Files/test.txt', 'r') as var:
+    f = var.read(100)
+    print(f, end="\n")
+    g = var.read(100)
+    print(g,end='')
+print("\n\n")
+with open('11 - File Objects - Reading and Writing to Files/test.txt', 'r') as var:
+    SizeToRead = 10
+    content = var.read(SizeToRead)
+    while len(content) > 0:
+        print(content,end="*")
+        content = var.read(SizeToRead)
+        time.sleep(0.1)
