2d7147c43d7c912bc16c455d0f67344960b2a51d
Cyrus Chung <chung.cyrus.0@gmail.com>
2026-01-17 19:34:50 +0000

    Update 1-10-2025.py

--- Pratices/1-10-2025.py
+++ Pratices/1-10-2025.py
@@ -1,3 +1,4 @@
+import random
 adj = ["happy","big","beautiful","small","old","new","good","bad","kind","tall"]
 nou = ["city","tree","house","keyboard","mouse","person","school","village","cable","car"]
 print(f"{random.choice(adj)}{random.choice(nou)}{random.randint(1,99)}")
